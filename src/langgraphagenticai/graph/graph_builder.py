2eec4fe7635db1cf5fda433a820549fec74ee8bb
sam02425 <saumilpatel2425@gmail.com>
2025-03-09 03:29:23 -0500

    chatbot with tools

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -22,6 +22,36 @@ class GraphBuilder:
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot", END)
 
+    def chatbot_with_tools_build_graph(self):
+        """
+        Builds an advanced chatbot graph with tool integration.
+        This method creates a chatbot graph that includes both a chatbot node
+        and a tool node. It defines tools, initializes the chatbot with tool
+        capabilities, and sets up conditional and direct edges between nodes.
+        The chatbot node is set as the entry point.
+        """
+        ## Define the tool and tool node
+
+        tools=get_tools()
+        tool_node=create_tool_node(tools)
+
+        ##Define LLM
+        llm = self.llm
+
+        # Define chatbot node
+        obj_chatbot_with_node = ChatbotWithToolNode(llm)
+        chatbot_node = obj_chatbot_with_node.create_chatbot(tools)
+
+        # Add nodes
+        self.graph_builder.add_node("chatbot", chatbot_node)
+        self.graph_builder.add_node("tools", tool_node)
+
+        # Define conditional and direct edges
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_conditional_edges("chatbot", tools_condition)
+        self.graph_builder.add_edge("tools","chatbot")
+
+
     def setup_graph(self, usecase: str):
         """Sets up the graph with a basic chatbot node.
         This method is a wrapper around the `build_chatbot_build_graph` method.
---
+++ src/langgraphagenticai/tools/search_tool.py
@@ -0,0 +1,17 @@
+from langchain.tools.tavily import TavilySearchResults
+from langgraph.prebuilt import ToolNode
+
+def get_tools():
+    """
+    Get the tools for the agent.
+    """
+    tools = TavilySearchResults(max_results=5,include_answer=True,
+    include_raw_content=True,
+    include_images=True)
+    return[tool]
+
+def create_tool_node(tools):
+    """
+    Create the tool node for the agent.
+    """
+    return ToolNode(tools=tools)
