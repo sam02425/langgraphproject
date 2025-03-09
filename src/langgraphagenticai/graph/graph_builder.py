902225f92a4245b4d7713ece5ce4772a1ca8f40c
sam02425 <saumilpatel2425@gmail.com>
2025-03-09 04:20:06 -0500

    chatbot with tool graph_builder update

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from src.langgraphagenticai.state.state import State
 from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
 from src.langgraphagenticai.nodes.chatbot_with_Tool_node import ChatbotWithToolNode
-from src.langgraphagenticai.tools.serach_tool import get_tools,create_tool_node
+from src.langgraphagenticai.tools.search_tool import get_tools,create_tool_node
 
 
 
